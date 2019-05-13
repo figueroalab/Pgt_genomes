d4e258902ad5b61790db5a3060454830481a08ed
[Feng Li, UMN] <lixx3627@umn.edu>
2019-05-13 09:52:48 -0500

    script to check the presence of telomeres

---
+++ scripts/telomere_detection.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import sys
+from Bio import SeqIO
+
+def search_telomere( seq ):
+    
+    if seq.find("CCCTAACCCTAACCCTAACCCTAACCCTAA") > 0:
+        return 0-seq.count("CCCTAA")
+    elif seq.find("TTAGGGTTAGGGTTAGGGTTAGGGTTAGGG" )> 0:
+        return seq.count("TTAGGG")
+    else: return 0        
+
+def main():
+    if len(sys.argv) < 2:
+        print("usage: %s in_fasta" % sys.argv[0], file=sys.stderr)                   
+        #print("usage", sys.argv[0], file=sys.stderr)
+        sys.exit()
+   
+    inhandle = open( sys.argv[1], "r" )
+    for record in SeqIO.parse( inhandle, "fasta"):
+        cnt = search_telomere( record.seq.upper() )
+        if abs(cnt)>0:
+            print(record.seq[:200])
+            print(record.seq[-200:])
+            print("%s: %d" % (record.id, abs(cnt))) 
+
+main()
